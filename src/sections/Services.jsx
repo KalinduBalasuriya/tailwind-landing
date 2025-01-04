4dc75e1d4c2d4658f3960a0a575a122f812c1d2c
Kalindu Balasuriya <61897592+KalinduBalasuriya@users.noreply.github.com>
2025-01-04 15:38:04 +0530

    Revert "Merge pull request #2 from KalinduBalasuriya/development"
    This reverts commit 446ead17ec7e1270a3f8f54e4d2bb7c0f8cf145f, reversing
    changes made to 62f4cbf22bd77b6964bc0f721fac682aef952b85.

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,15 +1,7 @@
 import React from "react";
-import { services } from "../constants";
-import ServiceCard from "../components/ServiceCard";
 
 const Services = () => {
-  return (
-    <section className="max-container flex justify-center flex-wrap gap-9">
-      {services.map((service) => (
-        <ServiceCard key={service.label} {...service} />
-      ))}
-    </section>
-  );
+  return <div>Services</div>;
 };
 
 export default Services;
